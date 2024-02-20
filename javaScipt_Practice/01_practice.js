b670782a966529fa88998178fa3de651eef1faa2
Souvik Das <143035243+sdas2508@users.noreply.github.com>
2024-02-20 18:52:23 +0000

    First day Exercise

---
+++ javaScipt_Practice/01_practice.js
@@ -0,0 +1,22 @@
+// const product = {
+// name: "Nikon Z5 Mirrorless Camera(24-70 mm lens 3.5-4.5 mm)",
+// price: 148499,
+// rating: "4.5*",
+// discount: "12%"
+// }
+// console.log(product['price']);
+
+// let a = 5;
+// let b = 3;
+// console.log("Your Ourput is = ", a+b)
+
+const profile = {
+    Name: "Souvik Das",
+    post: 89,
+    followers: "669K",
+    following: 7,
+    id: "@souvik2508",
+    designation: "Software Engineer",
+    Company: "Amazon"
+};
+console.log(profile['post']);
