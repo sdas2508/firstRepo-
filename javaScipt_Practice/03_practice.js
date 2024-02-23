a249d1f82f46c0de55209bb78a038770f9de54d0
Souvik Das <143035243+sdas2508@users.noreply.github.com>
2024-02-23 20:15:11 +0000

    03 day Practice

---
+++ javaScipt_Practice/03_practice.js
@@ -0,0 +1,23 @@
+ /* let str= "Souvik";
+
+for(let i of str){
+    console.log("str = ", i);
+} */ 
+
+ /* let str = "Bijoy Ekta Boro Paglachoda";
+let sum = 0;
+
+for(let i of str){
+    sum++;
+};
+console.log("Size = ",sum); */
+
+// print all the even value between  0 to 100
+
+let a = 0;
+let b = 100;
+for(let i = 0 ;i<b+1;i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+};
