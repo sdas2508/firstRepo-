d0028661fd2c6526fc8c66f318ed314895a32696
Souvik Das <143035243+sdas2508@users.noreply.github.com>
2024-03-01 20:26:15 +0000

    practiceDay_06

---
+++ javaScipt_Practice/06_practice.js
@@ -0,0 +1,15 @@
+// sum of two number
+
+/* const twoSum = ()=>{
+    let a= 51;
+    let b = 47;
+    let sum = a+b;
+    console.log("Sum of the Two  Number = ", sum);
+}
+twoSum();*/
+
+let arr = [47,63,79];
+
+arr.forEach((arr)=>{
+    console.log(arr*arr);
+});
